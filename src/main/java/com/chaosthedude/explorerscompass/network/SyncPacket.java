0274415cfd3965abe4d3cf48ba58ec743a1470a0
Matt Czyr <mattczyr@gmail.com>
2022-04-17 21:51:47 -0400

    Fix sanity check when loading structures on client side of sync packet (#5)

--- src/main/java/com/chaosthedude/explorerscompass/network/SyncPacket.java
+++ src/main/java/com/chaosthedude/explorerscompass/network/SyncPacket.java
@@ -42,13 +42,13 @@ public class SyncPacket extends PacketByteBuf {
 		
 		for (int i = 0; i < numStructures; i++) {
 			StructureFeature<?> structure = StructureUtils.getStructureForID(buf.readIdentifier());
+			int numDimensions = buf.readInt();
+			List<Identifier> dimensions = new ArrayList<Identifier>();
+			for (int j = 0; j < numDimensions; j++) {
+				dimensions.add(buf.readIdentifier());
+			}
 			if (structure != null) {
 				allowedStructures.add(structure);
-				int numDimensions = buf.readInt();
-				List<Identifier> dimensions = new ArrayList<Identifier>();
-				for (int j = 0; j < numDimensions; j++) {
-					dimensions.add(buf.readIdentifier());
-				}
 				dimensionsForAllowedStructures.put(structure, dimensions);
 			}
 		}
