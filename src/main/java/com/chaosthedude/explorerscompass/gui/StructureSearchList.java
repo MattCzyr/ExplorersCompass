3bab5e3f2c56d3cdab861cd8db3368adcc59b252
Matthew Czyr <mattczyr@gmail.com>
2022-10-02 15:09:38 -0400

    Add scroll bar to structure search list

--- src/main/java/com/chaosthedude/explorerscompass/gui/StructureSearchList.java
+++ src/main/java/com/chaosthedude/explorerscompass/gui/StructureSearchList.java
@@ -10,6 +10,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
 public class StructureSearchList extends AlwaysSelectedEntryListWidget<StructureSearchEntry> {
@@ -66,6 +67,19 @@ public class StructureSearchList extends AlwaysSelectedEntryListWidget<Structure
 			}
 		}
 
+		if (getMaxScroll() > 0) {
+			int left = getScrollbarPositionX();
+			int right = left + 6;
+			int height = (int) ((float) ((bottom - top) * (bottom - top)) / (float) getMaxPosition());
+			height = MathHelper.clamp(height, 32, bottom - top - 8);
+			int scrollbarTop = (int) getScrollAmount() * (bottom - top - height) / getMaxScroll() + top;
+			if (scrollbarTop < top) {
+				scrollbarTop = top;
+			}
+			
+			RenderUtils.drawRect(left, scrollbarTop, right, bottom, (int) (2.35F * 255.0F) / 2 << 24);
+			RenderUtils.drawRect(left, scrollbarTop, right, scrollbarTop + height, (int) (1.9F * 255.0F) / 2 << 24);
+		}
 	}
 
 	private int getRowBottom(int index) {
