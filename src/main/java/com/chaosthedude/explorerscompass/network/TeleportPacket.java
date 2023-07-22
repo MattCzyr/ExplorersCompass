1d99cf912121a1dfebdaa473dbcae8632b2a3597
Matthew Czyr <mattczyr@gmail.com>
2023-07-22 11:26:33 -0400

    Fix server hang when teleporting to X/Y coordinates with no blocks to stand on

--- src/main/java/com/chaosthedude/explorerscompass/network/TeleportPacket.java
+++ src/main/java/com/chaosthedude/explorerscompass/network/TeleportPacket.java
@@ -58,7 +58,7 @@ public class TeleportPacket {
 	private int findValidTeleportHeight(Level level, int x, int z) {
 		int upY = level.getSeaLevel();
 		int downY = level.getSeaLevel();
-		while (!(isValidTeleportPosition(level, new BlockPos(x, upY, z)) || isValidTeleportPosition(level, new BlockPos(x, downY, z)))) {
+		while ((!level.isOutsideBuildHeight(upY) || !level.isOutsideBuildHeight(downY)) && !(isValidTeleportPosition(level, new BlockPos(x, upY, z)) || isValidTeleportPosition(level, new BlockPos(x, downY, z)))) {
 			upY++;
 			downY--;
 		}
@@ -74,7 +74,7 @@ public class TeleportPacket {
 	}
 	
 	private boolean isValidTeleportPosition(Level level, BlockPos pos) {
-		return !level.isOutsideBuildHeight(pos) && isFree(level, pos) && isFree(level, pos.above()) && !isFree(level, pos.below());
+		return isFree(level, pos) && isFree(level, pos.above()) && !isFree(level, pos.below());
 	}
 	
 	private boolean isFree(Level level, BlockPos pos) {
