5be63279547c1fc53a556454a4d1fd4f015c91e8
Matthew Czyr <mattczyr@gmail.com>
2023-10-20 18:14:41 -0400

    Refactoring

--- src/main/java/com/chaosthedude/explorerscompass/ExplorersCompass.java
+++ src/main/java/com/chaosthedude/explorerscompass/ExplorersCompass.java
@@ -74,11 +74,7 @@ public class ExplorersCompass {
 	}
 
 	private void commonSetup(FMLCommonSetupEvent event) {
-		network = ChannelBuilder.named(new ResourceLocation(ExplorersCompass.MODID, ExplorersCompass.MODID))
-				.networkProtocolVersion(1)
-				.optionalServer()
-				.clientAcceptedVersions(Channel.VersionTest.exact(1))
-				.simpleChannel();
+		network = ChannelBuilder.named(new ResourceLocation(ExplorersCompass.MODID, ExplorersCompass.MODID)).networkProtocolVersion(1).optionalServer().clientAcceptedVersions(Channel.VersionTest.exact(1)).simpleChannel();
 
 		// Server packets
 		network.messageBuilder(CompassSearchPacket.class).encoder(CompassSearchPacket::toBytes).decoder(CompassSearchPacket::new).consumerMainThread(CompassSearchPacket::handle).add();
